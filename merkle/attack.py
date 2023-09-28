d8f8fc6f7d737566dc5f071e287fd46f10e07ec3
Yinmin Zhong <zhongyinmin@pku.edu.cn>
2023-09-28 11:00:07 -0400

    scenario 2 finished

--- merkle/attack.py
+++ merkle/attack.py
@@ -1,3 +1,9 @@
+import bitstring
+import hashlib
+
+def H(*args):
+    return hashlib.sha256(b''.join(args)).digest()
+
 class AttackOne:
     def __init__(self, s):
         self._store = s
@@ -13,15 +19,29 @@ class AttackOne:
 class AttackTwo:
     def __init__(self, s):
         self._store = s
+        arr = [b'0']*5050
+        self.attack_key = b'\xbf[\x81\xae\xffB\xdd\x01\xd5\x1f\xd2G\x94:`i\xc2\x13:\xeb\x84\x87\x8d\x19\x9c\xef\xa8FQ\x12\xb5\xc2'
+        self.attack_val = b'\xe3e'
+        self.fake_kvs = {}
+        for i in range(1, 5000):
+            fake_key = b''.join(arr[:i])
+            fake_value = b''.join(arr[i:])
+            path = bitstring.BitArray(H(fake_key))
+            if path[0] == False:
+                self.fake_kvs[fake_key] = fake_value
 
     def attack_fake_keys(self):
-        return {}
+        return self.fake_kvs.keys()
 
     def attack_key_value(self):
-        return b"hello", b"world"
+        return self.attack_key, self.attack_val
 
     def lookup(self, key):
-        return self._store.lookup(key)
+        proof = self._store.lookup(key)
+        proof.key = key
+        proof.val = self.fake_kvs[key]
+        proof.siblings = [self.attack_val]
+        return proof
 
 class AttackThree:
     def __init__(self, s):
