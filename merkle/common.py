79a2ab2310ea83c07117ff5412bb896098118ec6
Anna Arpaci-Dusseau <annaad@Annas-MacBook-Pro.local>
2023-09-28 11:39:22 -0400

    undoing accidental print statement added to merkle common.py

--- merkle/common.py
+++ merkle/common.py
@@ -22,12 +22,4 @@ class Proof:
         self.val = val
         self.siblings = siblings
 
-print("foo",traversal_path(b"foo"))
-print("hello",traversal_path(b"hello"))
-# print("foo", "".join(["1" if x else "0" for x in list(traversal_path(b"foo"))]))
-# print("hello","".join(["1" if x else "0" for x in list(traversal_path(b"hello"))]))
-
-# foo: 0010 1100 0010
-# hello: 0010 1100 1111
-
 
