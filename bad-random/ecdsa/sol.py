e4e52e9115b8fc83425c4f27fdcf78012590a630
Yinmin Zhong <zhongyinmin@pku.edu.cn>
2023-10-10 11:10:49 +0800

    Problem 2: Bad randomness in ECDSA

--- bad-random/ecdsa/sol.py
+++ bad-random/ecdsa/sol.py
@@ -1,5 +1,5 @@
 import hashlib
-from ecdsa import SigningKey, NIST256p
+from ecdsa import SigningKey, NIST256p, NIST192p, numbertheory
 from datetime import datetime, timedelta
 
 # Function to convert date string to the start and end timestamps for that date
@@ -31,4 +31,22 @@ def problem_1a(date_string, public_key):
 
 
 def problem_2b(sig1, sig2, Hm1, Hm2):
-    pass
+    order = NIST192p.order
+    # Extract r and s from signatures
+    r1, s1 = sig1
+    r2, s2 = sig2
+
+    if r1 != r2:
+        raise Exception("The r values are not the same; the same nonce might not have been used.")
+    
+    r = r1  # r is the same in both signatures because the same nonce was used
+
+    # Calculate k using s1, s2, Hm1, and Hm2
+    # alpha_t = (s1 - s2)^-1 * (Hm1 - Hm2) (mod order)
+    alpha_t = numbertheory.inverse_mod(s1 - s2, order) * (Hm1 - Hm2) % order
+
+    # Recover secret key using one of the signatures
+    # secret_key = (s1 * k - Hm1) * r^-1 (mod order)
+    secret_key = (s1 * alpha_t - Hm1) * numbertheory.inverse_mod(r, order) % order
+    
+    return secret_key
