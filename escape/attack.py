fab1c887ff8ff0c90652486b25af12416e30bda1
Yinmin Zhong <zhongyinmin@pku.edu.cn>
2023-11-04 03:17:00 +0800

    lab4: escape from python

--- escape/attack.py
+++ escape/attack.py
@@ -1,11 +1,35 @@
+import inspect
+import os
+
 def attack_one(guess):
-    return b''
+    secret = guess.__closure__[0].cell_contents
+    return secret
 
 def attack_two():
-    return b''
+    # Get the frame of the caller of the current function.
+    caller_frame = inspect.currentframe().f_back
+    # Access the local variables of the caller's frame.
+    local_vars_in_caller = caller_frame.f_locals
+    return local_vars_in_caller['secret']
 
 def attack_three():
-    return b''
+    def my_urandom(x):
+        return b'no_random'
+    os.urandom = my_urandom
+    return b'no_random'
 
 def attack_four():
+    # Get hash value h
+    caller_frame = inspect.currentframe().f_back
+    h = caller_frame.f_locals['h']
+
+    class my_sha256:
+        def __init__(self, x) -> None:
+            self.x = x
+        
+        def digest(self):
+            return h
+    import hashlib
+    hashlib.sha256 = my_sha256
+
     return b''
